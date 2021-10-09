801baa4238631bef8c5a5f2331475de73b4b7b9f
Maria Paz <pazguada@gmail.com>
2021-10-09 14:58:51 -0600

    Added cities style

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 
 export class Cities extends Component {
+    state = {
+    selectedCity:'VICTORIA'
+}
+    clickedButtonHandler = name => {
+        this.setState({selectedCity:name})
+    };
+
+   buttons = ['VICTORIA', 'CALGARY', 'TORONTO']
     render() {
+        const {selectedCity} = this.state
         return (
             <>
-            <div>
-                Victoria
-            </div>
-            <div>
-                Calgary
-            </div>
-            <div>
-                Toronto
-            </div>
+            {this.buttons.map((name, i)=>
+                <button key={i} name={name} onClick={() => this.clickedButtonHandler(name)} className={selectedCity === name ? 'activeButton' : ''}>{name}
+                    </button>
+            )
+            }
             </>
         )
     }
